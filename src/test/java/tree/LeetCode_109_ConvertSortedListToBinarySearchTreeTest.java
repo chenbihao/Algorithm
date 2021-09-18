577c8be8e7fd38aae266ac77b51c695779261542
chenbihao <994523036@qq.com>
2021-09-18 21:43:32 +0800

    LeetCode_109_有序链表转换二叉搜索树

---
+++ src/test/java/tree/LeetCode_109_ConvertSortedListToBinarySearchTreeTest.java
@@ -0,0 +1,40 @@
+package tree;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author: chenbihao
+ * @create: 2021/9/16
+ * @Description:
+ * @History:
+ */
+public class LeetCode_109_ConvertSortedListToBinarySearchTreeTest {
+
+    @Test
+    public void test() {
+
+        /**
+         * 输入：[-10, -3, 0, 5, 9]
+         * 输出树：
+         *       0
+         *      / \
+         *    -3   9
+         *    /   /
+         *  -10  5
+         *
+         * [0, -3, 9, -10, null, 5]
+         */
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+}
